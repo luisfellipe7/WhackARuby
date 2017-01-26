7332023d0c7fc4066641d19bb726ecd0a9f9df86
Luis Fellipe Furio Raphael <lfraphael@gmail.com>
2017-01-26 09:41:31 -0200

    Add files via upload

---
+++ whack.rb
@@ -0,0 +1,79 @@
+require 'gosu'
+
+class WhackARuby < Gosu::Window
+  def initialize
+    super 800, 600, false
+    self.caption = 'Whack the Ruby!'
+    @image = Gosu::Image.new(self, 'ruby.png', false)
+    @x = 200
+    @y = 200
+    @width = 50
+    @height = 43
+    @vx = 5
+    @vy = 5
+    @score = 0
+    @font = Gosu::Font.new(self, 'system', 30)
+    @visible = 0
+    @hammer_image = Gosu::Image.new(self, 'hammer.png', false)
+    @hit = 0
+    @playing = true
+    @start_time = 0
+  end
+
+  def update
+    if @playing
+      @x += @vx
+      @y += @vy
+      @visible -= 1
+      @time_left = (100 - ((Gosu.milliseconds - @start_time) / 1000)).to_s
+      @vx *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0
+      @vy *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
+      @visible = 50 if @visible < -10 && rand < 0.01
+      @playing = false if (Gosu.milliseconds - @start_time) > 100_000
+    end
+  end
+
+  def button_down(id)
+    if @playing
+      if id == Gosu::MsLeft
+        if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && (@visible >= 0)
+          @hit = 1
+          @score += 5
+        else
+          @hit = -1
+          @score -= 1
+        end
+      end
+    else
+      if id == Gosu::KbSpace
+        @playing = true
+        @visible = -10
+        @start_time = Gosu.milliseconds
+        @score = 0
+      end
+    end
+  end
+
+  def draw
+    if @hit == 0
+      c = Gosu::Color::NONE
+    elsif @hit == 1
+      c = Gosu::Color::GREEN
+    elsif @hit == -1
+      c = Gosu::Color::RED
+    end
+    draw_quad(0, 0, c, 800, 0, c, 800, 600, c, 0, 600, c)
+    @hit = 0
+    @image.draw(@x - @width / 2, @y - @height / 2, 1) if @visible > 0
+    @hammer_image.draw(mouse_x - 40, mouse_y - 10, 1)
+    @font.draw(@time_left, 20, 20, 2)
+    @font.draw(@score.to_s, 700, 20, 2)
+    unless @playing
+      @font.draw('Game Over', 300, 300, 3)
+      @font.draw('Press the Space Bar to Play Again', 175, 350, 3)
+      @visible = 20
+    end
+  end
+end
+window = WhackARuby.new
+window.show
